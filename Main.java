1e133606c1ac46961cff512b520f00fb9bbacf48
tatiana-polyakova <tanch88@mail.ru>
2020-06-03 23:20:31 +0300

    fixed #1

--- Main.java
+++ Main.java
@@ -1,11 +1,15 @@
+import java.util.logging.Logger;
+
 public class Main {
   public static void main(String[] args) {
+    Logger log = Logger.getLogger("My logger");
     BonusService service = new BonusService();
 
     long amount = 1000_60;
     boolean registered = true;
 
-    service.calculate(amount, registered);
+    long result = service.calculate(amount, registered);
+    log.info(String.valueOf(result));
   }
 }
 
